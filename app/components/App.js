02008feefa50fc478aaeb9a815fb3ebc528a8892
hongman <hm@beyondcreative.co.kr>
2016-06-12 05:18:14 +0900

    changed

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import ReactPlayer from 'react-player';
+import ReactPlayer from '../../node_modules/react-player';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header/>
-        return <ReactPlayer url='https://www.youtube.com/watch?v=9Hztx59K6m8' playing />
+        <ReactPlayer url='https://www.youtube.com/watch?v=9Hztx59K6m8' playing />
         {this.props.children}
         <Footer/>
       </div>
